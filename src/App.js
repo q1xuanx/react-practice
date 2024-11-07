c06a9852404a09b2a374767dbc3011380ee17b91
nhan <nhoang2929@gmail.com>
2024-11-07 22:21:39 +0700

    add close button to hide component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
 import { useState } from "react";
 
- 
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-
-export default function App(){
+export default function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
   const previousEvent = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + messages.length) % messages.length)
-  }; 
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + messages.length) % messages.length
+    );
+  };
   const nextEvent = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    setCurrentIndex((nextIndex) => (nextIndex + 1) % messages.length);
+  };
+  const closeComponent = () => {
+    setIsOpen(!isOpen);
   }
-  return <div className="steps">
-    <div className="numbers">
-      {messages.map((_, index) => (
-        <div key={index} className={index === currentIndex ? "active" : ""}>
-          {index + 1}
-        </div>
-      ))}
+  return (
+    <div>
+    <button className="close" onClick={closeComponent}>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            {messages.map((_, index) => (
+              <div
+                key={index}
+                className={index === currentIndex ? "active" : ""}
+              >
+                {index + 1}
+              </div>
+            ))}
+          </div>
+          <Displaytitle index={currentIndex} />
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={previousEvent}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={nextEvent}
+            >
+              Next
+            </button>
+          </div>
+        </div>)
+      }
     </div>
-    <Displaytitle index={currentIndex}/>
-    <div className="buttons">
-      <button style={{backgroundColor: '#7950f2', color: "#fff"}} onClick={previousEvent}>Previous</button>
-      <button style={{backgroundColor: '#7950f2', color: "#fff"}} onClick={nextEvent}>Next</button>
-    </div>
-  </div>; 
+  );
 }
-function Displaytitle(props){
-  return <p className="message">Step {props.index + 1}: {messages[props.index]}</p>
+function Displaytitle(props) {
+  return (
+    <p className="message">
+      Step {props.index + 1}: {messages[props.index]}
+    </p>
+  );
 }
